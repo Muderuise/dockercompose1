447dff075ddbd98845918ca3a60e2198e18dcb6c
Muderuise <83969189+Muderuise@users.noreply.github.com>
2021-05-27 01:53:54 +0200

    ey4

---
+++ creaTablas.sql
@@ -0,0 +1,4 @@
+CREATE TABLE persona (id int, nombre varchar(255), apellido varchar(255), PRIMARY KEY (id));
+INSERT INTO persona (id,nombre,apellido) values(0,'Sofia','Perez');
+INSERT INTO persona (id,nombre,apellido) values(1,'Carlos','Ruiz');
+INSERT INTO persona (id,nombre,apellido) values(2,'Maria','Garcia');
